ba5d87e3ec2c7c22d8dcd62ed6a33ea4795f8c6b
tpotp <thepowerofthepuduchile@gmail.com>
2026-03-23 15:32:39 -0300

    Auto deploy 23/03/2026 15:32:38,96

--- deploy.bat
+++ deploy.bat
@@ -3,22 +3,11 @@ echo ===============================
 echo Deploy a GitHub / Vercel
 echo ===============================
 
-REM Moverse a la carpeta del proyecto
-cd /d %~dp0
-
-REM Ver estado
-git status
-
-REM Agregar cambios
 git add .
-
-REM Commit con fecha y hora
 git commit -m "Auto deploy %date% %time%"
-
-REM Push a GitHub
-git push
+git push origin main
 
 echo ===============================
-echo Deploy enviado a Vercel 🚀
+echo Deploy enviado a Vercel
 echo ===============================
 pause
