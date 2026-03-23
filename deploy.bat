aafde9efe80ef1abca8330825e9830a6b62b0e21
tpotp <thepowerofthepuduchile@gmail.com>
2026-03-23 15:52:07 -0300

    Auto deploy 23/03/2026 15:52:07,64

--- deploy.bat
+++ deploy.bat
@@ -3,11 +3,25 @@ echo ===============================
 echo Deploy a GitHub / Vercel
 echo ===============================
 
+echo.
+echo Agregando cambios...
 git add .
+
+echo.
+echo Commit...
 git commit -m "Auto deploy %date% %time%"
+
+echo.
+echo Sincronizando con GitHub...
+git pull origin main --allow-unrelated-histories
+
+echo.
+echo Subiendo a GitHub...
 git push origin main
 
+echo.
 echo ===============================
 echo Deploy enviado a Vercel
 echo ===============================
+echo Espera 1-2 minutos y recarga con CTRL+F5
 pause
